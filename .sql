3d67dd5c34c7ec9a0c697e27da321ebe8eb33dd8
bobbiaditya <bobbi.aditya@gmail.com>
2020-04-20 21:42:00 +0700

    DB BISAAAA

---
+++ .sql
@@ -0,0 +1,40 @@
+CREATE TABLE pabrik (
+  id_pabrik int NOT NULL,
+  nama_pabrik varchar(100) NOT NULL,
+  kode_pabrik varchar(5) NOT NULL,
+  harga_pasir decimal(13,2) NOT NULL,
+  updated_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  created_at datetime NOT NULL DEFAULT '0000-00-00 00:00:00'
+)
+
+ALTER TABLE pabrik
+  ADD PRIMARY KEY (id_pabrik);
+
+
+
+
+CREATE TABLE pemilik_truk (
+  id_pemilik int NOT NULL,
+  nama_pemilik varchar(100) NOT NULL,
+  updated_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  created_at datetime NOT NULL DEFAULT '0000-00-00 00:00:00'
+)
+
+ALTER TABLE pemilik_truk
+  ADD PRIMARY KEY (id_pemilik);
+
+CREATE TABLE supir_truk (
+  id_supir int NOT NULL PRIMARY KEY,
+  id_pemilik int NOT NULL FOREIGN KEY REFERENCES pemilik_truk(id_pemilik),
+  nama_supir varchar(100) NOT NULL,
+  nopol varchar(10) NOT NULL,
+  updated_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  created_at datetime NOT NULL DEFAULT '0000-00-00 00:00:00'
+
+  CREATE TABLE users (
+  id_user int NOT NULL,
+  username varchar(100) NOT NULL,
+  pwd  varchar(100) NOT NULL,
+  updated_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  created_at datetime NOT NULL DEFAULT '0000-00-00 00:00:00'
+)
