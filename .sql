90c1aff13c9294e0c9704afbb8462e4f507c5107
bobbiaditya <bobbi.aditya@gmail.com>
2020-04-21 20:17:07 +0700

    register bro

--- .sql
+++ .sql
@@ -30,9 +30,10 @@ CREATE TABLE supir_truk (
   nopol varchar(10) NOT NULL,
   updated_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   created_at datetime NOT NULL DEFAULT '0000-00-00 00:00:00'
+)
 
   CREATE TABLE users (
-  id_user int NOT NULL,
+  id_user int IDENTITY(1,1) NOT NULL,
   username varchar(100) NOT NULL,
   pwd  varchar(100) NOT NULL,
   updated_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
