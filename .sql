e563cc2e0a728e8f21dd86f142575b4b181dc84d
bobbiaditya <bobbi.aditya@gmail.com>
2020-04-22 10:31:27 +0700

    update auto increment

--- .sql
+++ .sql
@@ -1,5 +1,5 @@
 CREATE TABLE pabrik (
-  id_pabrik int NOT NULL,
+  id_pabrik int IDENTITY(1,1) NOT NULL,
   nama_pabrik varchar(100) NOT NULL,
   kode_pabrik varchar(5) NOT NULL,
   harga_pasir decimal(13,2) NOT NULL,
@@ -14,7 +14,7 @@ ALTER TABLE pabrik
 
 
 CREATE TABLE pemilik_truk (
-  id_pemilik int NOT NULL,
+  id_pemilik int IDENTITY(1,1) NOT NULL,
   nama_pemilik varchar(100) NOT NULL,
   updated_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   created_at datetime NOT NULL DEFAULT '0000-00-00 00:00:00'
@@ -24,7 +24,7 @@ ALTER TABLE pemilik_truk
   ADD PRIMARY KEY (id_pemilik);
 
 CREATE TABLE supir_truk (
-  id_supir int NOT NULL PRIMARY KEY,
+  id_supir int IDENTITY(1,1) NOT NULL PRIMARY KEY,
   id_pemilik int NOT NULL FOREIGN KEY REFERENCES pemilik_truk(id_pemilik),
   nama_supir varchar(100) NOT NULL,
   nopol varchar(10) NOT NULL,
