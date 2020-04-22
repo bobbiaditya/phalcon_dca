bfb3c0fba63a3e10c8af9313d7f892ffdded3239
bobbiaditya <bobbi.aditya@gmail.com>
2020-04-22 11:53:47 +0700

    crud pemilik truk done kurang eror validasi

--- app/controllers/PemilikTrukController.php
+++ app/controllers/PemilikTrukController.php
@@ -8,8 +8,63 @@ class PemilikTrukController extends ControllerBase
 
     public function indexAction()
     {
+        $this->view->pemilik = PemilikTruk::find();
+    }
+
+    public function tambahAction()
+    {
 
     }
 
+    public function prosesAction()
+    {
+        $pem = new PemilikTruk();
+
+        $pem->assign(
+            $this->request->getPost(),
+            [
+                'nama_pemilik',
+            ]
+        );
+        $pem->updated_at = date('Y-m-d h:i:sa');
+        $pem->created_at = date('Y-m-d h:i:sa');
+
+        $success = $pem->save();
+
+        $this->response->redirect('/pemiliktruk');
+
+    }
+
+    public function editAction($id)
+    {
+        $pem = PemilikTruk::findFirstById_pemilik($id);
+        $this->view->pemilik = $pem;       
+    }
+
+    public function updateAction($id)
+    {
+        $pem = PemilikTruk::findFirstById_pemilik($id);
+
+        $pem->assign(
+            $this->request->getPost(),
+            [
+                'nama_pemilik',
+            ]
+        );
+        $pem->updated_at = date('Y-m-d h:i:sa');
+        
+        $success = $pem->save();
+
+        $this->response->redirect('/pemiliktruk');
+    }
+
+    public function hapusAction($id)
+    {
+        $pem = PemilikTruk::findFirstById_pemilik($id);
+
+        $success = $pem->delete();
+
+        $this->response->redirect('/pemiliktruk');
+    }
 }
 
